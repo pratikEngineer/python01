0e8d801e5aa60aa9fb8a71be8ba257cd6c840a7e
pratikEngineer <pratikkokate633@gmail.com>
2026-01-15 18:07:15 +0530

    First commit for py

---
+++ if.py
@@ -0,0 +1,35 @@
+# a=2
+# b=7
+# c=4
+# if a>b and a>c:
+#     print("A is greater")
+# elif b>a and b>c:
+#     print("B is greater")
+# else:
+#     print("C is greater")
+
+#
+# age=int(input("Enter your age:"))
+# if age>=18:
+#     print("Eligible to vite")
+# else:
+#     print("Not eligible to vote")
+
+# fruits=["Appli","Mango","Pineapple"]
+# for fruit in fruits:
+#     print(fruit)
+#
+#
+# apple=1
+# while apple <=7:
+#     print(apple)
+#     apple +=1
+#
+nested_list=[[1,2,3],[5,6,7]]
+print(nested_list[1][2])
+
+nested_list=[[1,2,3],[4,5,6]]
+print(nested_list[1][2])
+
+nested_list=[[1,2,3],[3,4,4]]
+print(nested_list[1][2])
