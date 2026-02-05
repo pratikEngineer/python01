2226586f3aae0c479cd3674ebca47cd541e35d13
pratikEngineer <pratikkokate633@gmail.com>
2026-02-05 15:09:42 +0530

    spark

--- practice/hello.py
+++ practice/hello.py
@@ -1,1 +1,11 @@
 print("hello world")
+
+
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+
+def add(a:int,b:int):
+    return a+b
+
+result=(add(a,b))
+print(f"The sum of {a} and {b} is {result}")
